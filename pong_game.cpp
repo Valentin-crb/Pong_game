2076c5e8c670b272c50f259a3a3fbfaa99f6dc7f
Valentin-crb <curbat.valentin@gmail.com>
2025-03-29 14:34:27 +0200

    am desenat o paleta

--- pong_game.cpp
+++ pong_game.cpp
@@ -1,57 +1,61 @@
 #include <iostream>
 #include <conio.h>
 using namespace std;
-void draw_table(char tabla[50][200]);
-void draw_palet(char paleta[8][3]);
-
+const int rows=50, cols=200;
+const int xborder1=30, xborder2=160; //marginilie tablei pe axa Ox
+const int yborder1=10, yborder2=49;  //mariginile tablei pe axa Oy
+void draw_table(char tabla[rows][cols]);
+void draw_palet(char paleta[rows][cols]);
+void move_palet(char paleta[rows][cols], int posY);
 int main()
 {
-    char tabla[50][200];  
-    char paleta[8][3];   
+    int posy = (yborder1 + yborder2) / 2;
+    char tabla[rows][cols];  
+    char paleta[rows][cols];   
     draw_table(tabla);
-
+    draw_palet(tabla);
+    move_palet(tabla, posy);
     return 0;
 }
-void draw_table(char tabla[50][200])
+void draw_table(char tabla[rows][cols])
 {
-    for (int i = 0; i < 50; i++)
-        for (int j = 0; j < 200; j++)
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
             tabla[i][j] = ' ';
-    for (int j = 30; j <= 160; j++)
+    for (int j = xborder1; j <= xborder2; j++)
     {
-        tabla[10][j] = '-';
-        tabla[30][j] = '-';
+        tabla[yborder1][j] = '-';
+        tabla[yborder2][j] = '-';
     }
-    for (int i = 10; i <= 30; i++)
+    for (int i = yborder1; i <= yborder2; i++)
     {
-        tabla[i][30] = '|';
-        tabla[i][160] = '|';
-        tabla[i][100] = '|'; // mijlocul tablei
-    }
-    for (int i = 0; i < 50; i++)
-    {
-        for (int j = 0; j < 200; j++)
-            cout << tabla[i][j];
-        cout << endl;  // Afișăm o linie nouă după fiecare linie a tablei
+        tabla[i][xborder1] = '|';
+        tabla[i][xborder2] = '|';
+        tabla[i][(xborder1+xborder2)/2] = '|'; // mijlocul tablei
     }
 }
-void draw_palet(char paleta[8][3])
+void draw_palet(char paleta[rows][cols])
 {
-    for (int i = 0; i < 8; i++) {
-        paleta[i][0] = '|';
-        paleta[i][2] = '|';
+    for (int i = (yborder1+yborder2)/2; i < (yborder1+yborder2)/2 + 5; i++) { //lungimea paletei este de 5
+        paleta[i][xborder1+3] = '#';
+        paleta[i][xborder1+5] = '#';
     }
-    for (int j = 0; j < 3; j++) {
-        paleta[0][j] = '-';
-        paleta[7][j] = '-';
+    for (int j = xborder1+3; j < xborder1+6; j++) { //latimea paletei de 3
+        paleta[(yborder1+yborder2)/2-1][j] = '#';
+        paleta[(yborder1+yborder2)/2 + 4][j] = '#';
+    }
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++)
+            cout << paleta[i][j];
+        cout << '\r' << flush;  // afisez tabla
     }
 }
-void move_palet(char paleta[8][3], int& posY) {
+void move_palet(char paleta[rows][cols], int posY) {
     if (_kbhit()) {  // Verifică dacă o tastă a fost apăsată
         char n = _getch();  // Obține tasta apăsată
         if (n == 'w' && posY > 10)
             posY--;
-        else if (n == 's' && posY < 40 - 8)  // 8 este înălțimea paletei
+        else if (n == 's' && posY < 40 - 5)  // rows este înălțimea paletei
             posY++;
         }
 }
